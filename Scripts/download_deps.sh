12a60933a213cbc253594b30247ea74300646b42
joolurik <kartheekjooluri0405@gmail.com>
2020-09-03 17:04:56 -0700

    Update download_deps.sh

--- Scripts/download_deps.sh
+++ Scripts/download_deps.sh
@@ -2,6 +2,6 @@ if [ -d "Submodules" ]; then
     echo "Already cloned submodules. Delete the submodules directory to re-clone submodules."
 else
     echo "Submodules does not exist, cloning submodules."
-    git clone https://github.com/facebook/fishhook Submodules/fishhook
-    git clone https://github.com/google/eDistantObject Submodules/eDistantObject
+    git clone https://github.com/joolurik/fishhook Submodules/fishhook
+    git clone https://github.com/joolurik/eDistantObject Submodules/eDistantObject
 fi
